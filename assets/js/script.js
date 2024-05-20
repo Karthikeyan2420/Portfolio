58ffd0a3cc54cd44829f21a1acecb77f0af01e3a
Karthikeyan2420 <84924959+Karthikeyan2420@users.noreply.github.com>
2024-05-20 14:04:36 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+
+
+// element toggle function
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+
+
+
+
+
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function () {
+
+    for (let i = 0; i < pages.length; i++) {
+      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        pages[i].classList.add("active");
+        navigationLinks[i].classList.add("active");
+        window.scrollTo(0, 0);
+     
+      } else {
+        pages[i].classList.remove("active");
+        navigationLinks[i].classList.remove("active");
+      }
+    }
+
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const items = document.querySelectorAll(".timeline-item");
+
+  function animateItems() {
+    items.forEach((item, index) => {
+      setTimeout(() => {
+        item.classList.add("animate");
+      }, index * 500); // Adjust the delay as needed
+    });
+  }
+
+animateItems()
+});
